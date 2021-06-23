ce41deffc88bb43f32b9f357c817e9063caa241e
john-cits <86371058+john-cits@users.noreply.github.com>
2021-06-23 13:11:38 -0400

    Added a comment
    A Quick comment to look at changes in GitHub Desktop

--- Assets/Scripts/Damagers.cs
+++ Assets/Scripts/Damagers.cs
@@ -6,6 +6,8 @@ public class Damagers : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Check if this is the player
+        // If it is, tell it to take damage
         if (collision.tag == "Player") collision.GetComponent<Player>().TakeDamage();
     }
 }
